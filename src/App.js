f152c7a2d0c301160658b04be92fb51f4a1cb25d
Muamer Alickovic <alickovicmuamer1@gmail.com>
2024-01-14 15:11:44 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+    <p>App deployed for Cloud Computing projct</p>
+    <p>Muamer Alickovic</p>
+    </div>
+  );
+}
+
+export default App;
